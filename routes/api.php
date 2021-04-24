f826ae33738c625ddf9306d3bf238a4978d7ef11
kleber <kleber107@gmail.com>
2021-04-24 20:31:33 -0300

    initial

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('auth/login', 'AuthController@login');
 Route::post('auth/recoverPassword', 'AuthController@recoverPassword');
+Route::get('auth/me', 'AuthController@me');
 
 Route::group(['middleware' => 'apiJwt'], function () {
     Route::post('auth/logout', 'AuthController@logout');
     Route::post('auth/refresh', 'AuthController@refresh');
     Route::post('auth/validate', 'AuthController@tokenIsValidate');
-    Route::get('auth/me', 'AuthController@me');
     Route::put('auth/changePassword', 'AuthController@changePassword');
 });
